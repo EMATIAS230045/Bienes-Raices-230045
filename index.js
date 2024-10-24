feeb20d25f5709c79c2817159c92098379f69cc2
Erick Matias Granillo Mejia <230045@utxicotepec.edu.mx>
2024-10-24 17:31:15 -0600

    Practica 10 Actualizacion2
    Actualizacion del archivo json

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hola desde code.js");
+const express = require('express')
+ 
+//Creacion de la app
+
+const app = express()
+//Routing 
+app.get('/', function(req, res){
+    res.send('Hola mundo desde express')
+})
+
+app.get('/', function(req, res){
+    res.send('Nuestra informacion')
+})
+
+//Definir un puerto y arrancar el proyecto
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Él servidor esta funcionando correctamente en el puerto ${port}`)
+});
